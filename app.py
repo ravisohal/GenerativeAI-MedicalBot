da2577da87ba79817d2f18bef82a33b9e516bda4
Ravi Sohal <ravi.sohal@yahoo.com>
2025-03-02 15:39:10 -0500

    Clean up app.py

--- app.py
+++ app.py
@@ -21,9 +21,6 @@ load_dotenv()
 PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
 OPENAI_API_KEY=os.environ.get('OPENAI_API_KEY')
 
-os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
-os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-
 #print(PINECONE_API_KEY)
 #print(OPENAI_API_KEY)   
 
